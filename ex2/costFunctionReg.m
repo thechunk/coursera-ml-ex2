715f7ddc034d865fdec4c487d19fd0d766107c59
Russell Cheung <thechunk@gmail.com>
2015-12-19 12:51:19 +0800

    Cost fn regularized

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,7 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y);
+regt = sum(theta(2:end, :) .^ 2) * lambda / (2 * m);
+J = J + regt;
 
+grad = grad + (lambda / m) * [0; theta(2:end, :)];
 
 
 
